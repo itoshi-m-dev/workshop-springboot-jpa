1f6e006898453e09b23b4acc5fcccf327eaaa8c8
itoshi-m-dev <emanuelkalison@gmail.com>
2025-09-24 16:29:26 -0300

    Project created

---
+++ src/main/java/com/emanueljpa/projectspringv1/Projectspringv1Application.java
@@ -0,0 +1,13 @@
+package com.emanueljpa.projectspringv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projectspringv1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projectspringv1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/emanueljpa/projectspringv1/Projectspringv1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emanueljpa.projectspringv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projectspringv1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
