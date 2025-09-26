d724093405132f709c0bdcd0f70c71f559481d7d
itoshi-m-dev <emanuelkalison@gmail.com>
2025-09-26 14:53:33 -0300

    Jpa repository, dependency injection, database seeding

---
+++ src/main/java/com/emanueljpa/projectspringv1/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.emanueljpa.projectspringv1.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.emanueljpa.projectspringv1.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+		
+	
+	
+	
+}
