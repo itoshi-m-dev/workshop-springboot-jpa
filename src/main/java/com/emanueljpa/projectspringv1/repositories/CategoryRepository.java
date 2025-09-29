afc2c010448178ad50c5f2ee4e1f50e791b72ddf
itoshi-m-dev <emanuelkalison@gmail.com>
2025-09-29 15:15:56 -0300

    Category entity

---
+++ src/main/java/com/emanueljpa/projectspringv1/repositories/CategoryRepository.java
@@ -0,0 +1,13 @@
+package com.emanueljpa.projectspringv1.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.emanueljpa.projectspringv1.entities.Category;
+
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+		
+	
+	
+	
+}
---
+++ src/main/java/com/emanueljpa/projectspringv1/services/CategoryService.java
@@ -0,0 +1,33 @@
+package com.emanueljpa.projectspringv1.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.emanueljpa.projectspringv1.entities.Category;
+import com.emanueljpa.projectspringv1.repositories.CategoryRepository;
+
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	
+	public List<Category> findAll() {
+		return repository.findAll();
+	
+		
+	}
+	
+	public Category findById(Long id) {
+		Optional <Category> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+	
+
+}
