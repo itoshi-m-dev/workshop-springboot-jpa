a6db2581c12ade5ff4ab27188ee92cbccb322601
itoshi-m-dev <emanuelkalison@gmail.com>
2025-09-29 15:37:56 -0300

    Product entity

---
+++ src/main/java/com/emanueljpa/projectspringv1/repositories/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.emanueljpa.projectspringv1.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.emanueljpa.projectspringv1.entities.Product;
+
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+		
+	
+	
+	
+}
